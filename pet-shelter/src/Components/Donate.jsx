64a170352d6f536e4703a7fccbc4520bbf1313d8
Jeffrey Arbelaez <Jeffreyarbelaez@yahoo.com>
2025-04-19 12:53:24 -0500

    successfully added state to the donate page

--- pet-shelter/src/Components/Donate.jsx
+++ pet-shelter/src/Components/Donate.jsx
@@ -1,67 +1,132 @@
-import "../css/donate.css"
+import { useState } from "react";
+import "../css/donate.css";
 
-function Donate (){
+function Donate() {
+const [name, setName] = useState ("")
+const [lastName, setLastName] = useState ("")
+const [email, setEmail] = useState ("")
+const [donation,setDonation] = useState ("")
 
-    return(
-        <div class="donation-container">
-        <div class="donation-grid">
-        <div class="donation-price">
-            <h1 class=" price-title"> How your donations help</h1>
-            
-            
-            <div class="donation-p">
-            <h3 > $50 </h3>
+const[ submitted, setSubmitted] =useState (false)
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  console.log({name, lastName,email,donation})
+  setSubmitted(true)
+  setName("")
+  setLastName("")
+  setEmail("")
+  setDonation("") 
+}
+
+  return (
+    <div className="donation-container">
+      <div className="donation-grid">
+        <div className="donation-price">
+          <h1 className=" price-title"> How your donations help</h1>
+
+          <div className="donation-p">
+            <h3> $50 </h3>
             <p> - Supplies for a month for any pet</p>
             <p> - Covers basic vaccinations for a small animal </p>
             <h3> $100 </h3>
             <p> - Fund a veterinary wellness check for a pet</p>
-            <p> - Supply a cozy bed, toys, and enrichment materials for a fostered animal</p>
+            <p>
+              {" "}
+              - Supply a cozy bed, toys, and enrichment materials for a fostered
+              animal
+            </p>
             <h3> $150 </h3>
-            <p> - Cover flea, tick and parasite prevention for a pet for several months</p>
-            <p> - Provides heat lamps, enclosures or specialized diets for reptiles and exotic pets</p>
+            <p>
+              {" "}
+              - Cover flea, tick and parasite prevention for a pet for several
+              months
+            </p>
+            <p>
+              {" "}
+              - Provides heat lamps, enclosures or specialized diets for
+              reptiles and exotic pets
+            </p>
             <h3> $250 </h3>
             <p> - Pays for a spay/neuter surgery for a dog, cat or rabbit</p>
             <p> - Covers emergency medical care for an injured or sick pet</p>
             <h3> $500 </h3>
             <p> - Funds a life saving surgery for intensive care treatment</p>
             <p> - Supplies for about 10 small animals or 5 big animals</p>
+          </div>
         </div>
-        </div>
-      <div class ="donation-container-container">
-    <h1 class="donation-title">Donate to Wings, Tails and Scales</h1>
-    <form class="form">
-      <div class="form-container-container">
-      <div class="form-container">
-      <div class="form-lines">
-        <label for="first-name"> First Name</label>
-        <input class="inputs" type="name" name="first-name" id="first-name" placeholder="first name" />
-      </div>
-      <div class="form-lines">
-        <label for="last-name"> Last Name </label>
-        <input class="inputs" type="name" name="last-name" id="last-name" placeholder="last name"/>
-      </div>
-      <div class="form-lines">
-        <label for="email"> Email Address </label>
-        <input class="inputs" type="email" name="email" id="email" placeholder="email" />
-      </div>
+        <div className="donation-container-container">
+          <h1 className="donation-title">Donate to Wings, Tails and Scales</h1>
+          <form onSubmit={handleSubmit} className="form">
+            <div className="form-container-container">
+              <div className="form-container">
+                <div className="form-lines">
 
-      <div class="form-lines">
-        <label for="last-name"> Donation Amount </label>
-        <input class="inputs" type="number" name="donation" id="donation" placeholder="Donation Amount"/>
-      </div>
+                  <label htmlFor="first-name"> First Name</label>
+                  <input
+                    className="inputs"
+                    type="text"
+                    name="first-name"
+                    id="first-name"
+                    placeholder="first name"
+                    value={name}
+                    onChange={(e)=> setName(e.target.value)}
+                    required
+                  />
+                </div>
+                <div className="form-lines">
+                  <label htmlFor="last-name"> Last Name </label>
+                  <input
+                    className="inputs"
+                    type="text"
+                    name="last-name"
+                    id="last-name"
+                    placeholder="last name"
+                    value={lastName}
+                    onChange={(e) => setLastName(e.target.value)}
+                    required
+                  />
+                </div>
+                <div className="form-lines">
+                  <label htmlFor="email"> Email Address </label>
+                  <input
+                    className="inputs"
+                    type="email"
+                    name="email"
+                    id="email"
+                    placeholder="email"
+                    value={email}
+                    onChange={(e)=> setEmail(e.target.value)}
+                    required
+                  />
+                </div>
 
-      </div>
-      <div class="submit-button-container">
-        <div class="submit-button-div">
-          <button class="submit-button">Submit</button>
+                <div className="form-lines">
+                  <label htmlFor="last-name"> Donation Amount $ </label>
+                  <input
+                    className="inputs"
+                    type="number"
+                    name="donation"
+                    id="donation"
+                    placeholder="Donation Amount"
+                    value={donation}
+                    onChange={(e)=> setDonation (e.target.value)}
+                    required
+                  />
+                </div>
+              </div>
+              <div className="submit-button-container">
+                <div className="submit-button-div">
+                  <button className="submit-button">Submit</button>
+                  {submitted && <p> The form has successfully been submitted, Thank you for your donation!</p>}
+                </div>
+              </div>
+            </div>
+          </form>
         </div>
       </div>
-      </div>
-    </form>
     </div>
-    </div>
-    </div>
-    )
+  );
 }
 
-export default Donate
+export default Donate;
